c024d0b102faf77f6bf01c4715c828502e39ab72
RodrigoLupo <rodrigo.lupo@tecsup.edu.pe>
2024-10-02 18:54:19 -0500

    Initial Commit

---
+++ LupoColeccionJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LupoColeccionJuegos
+//
+//  Created by Rodrigo Diegojosue Lupo Cruz on 2/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
